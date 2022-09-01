550f9837b7db521c45a05e370ceb96d76f39ba5b
Joshva115 <Joshua.Chantler@hotmail.com>
2022-09-01 15:51:12 +1000

    Updates

---
+++ precache-manifest.f87fce4a0876a72b3f150f8aeca451bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1cfce297c0dbaa1f0b52",
+    "url": "/react/static/css/main.37801548.chunk.css"
+  },
+  {
+    "revision": "1cfce297c0dbaa1f0b52",
+    "url": "/react/static/js/main.07c35b3e.chunk.js"
+  },
+  {
+    "revision": "2c703a42966522d7a55d",
+    "url": "/react/static/js/runtime~main.f8feb895.js"
+  },
+  {
+    "revision": "c66993b7d603c76a6db0",
+    "url": "/react/static/css/2.e9d8324f.chunk.css"
+  },
+  {
+    "revision": "c66993b7d603c76a6db0",
+    "url": "/react/static/js/2.fe83977b.chunk.js"
+  },
+  {
+    "revision": "a65f0960c121f766bc7a",
+    "url": "/react/static/js/3.bcc3ad52.chunk.js"
+  },
+  {
+    "revision": "7320d1820b74943644bef2f447e30871",
+    "url": "/react/index.html"
+  }
+];
